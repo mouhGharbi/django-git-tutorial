2c63a730e7614f140fed879342fe13fc8a1cd46f
Gharbi Mohamed <mohamed@gh>
2019-02-06 11:09:08 +0100

    ADD in poll/models.py two models Question and Choice

--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    question_content = models.CharField(max_length=200)
+    question_published_date = models.DateTimeField("published date")
+
+
+class Choice(models.Model):
+    choice_question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_content = models.CharField(max_length=200)
+    choice_tally = models.IntegerField(default=0)
