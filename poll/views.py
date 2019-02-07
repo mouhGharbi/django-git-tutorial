0a25c7c7fb038e1f3e94eb2eb338afb57519b68e
Gharbi Mohamed <mohamed@gh>
2019-02-07 14:23:04 +0100

    try the old Http404 for the view

--- poll/views.py
+++ poll/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import renderf
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from .models import Question
 # Create your views here.
 
+def index(request):
+    questions = Question.objects.order_by('-question_published_date')[:5]
+    
+    return render(request, 'poll/index.html', {'questions': questions})
 def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("the question you asked for does not exist please check the url")
     return HttpResponse("you are looking at question %s" % question_id)
 
 def results(request, question_id):
