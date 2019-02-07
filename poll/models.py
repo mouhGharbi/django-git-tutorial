5edbc8addf2bf1dd2a85addc4cd8b5a6ad87f0fb
Gharbi Mohamed <mohamed@gh>
2019-02-07 12:54:32 +0100

    the ignored file

--- poll/models.py
+++ poll/models.py
@@ -1,13 +1,20 @@
 from django.db import models
-
+from django.utils import timezone
+import datetime
 # Create your models here.
 
 class Question(models.Model):
     question_content = models.CharField(max_length=200)
     question_published_date = models.DateTimeField("published date")
-
+    
+    def __str__(self):
+        return self.question_content
+    def was_published_recently(self):
+        return self.question_published_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     choice_question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_content = models.CharField(max_length=200)
     choice_tally = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_content
