2d913c991b98e9c16ce26963897ffc24649c411a
Gharbi Mohamed <mohamed@gh>
2019-02-07 13:01:45 +0100

    create the first views for pull app with HttpResponse

--- poll/views.py
+++ poll/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import renderf
+from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
-    return render(request,
-            'poll/home.html')
+def detail(request, question_id):
+    return HttpResponse("you are looking at question %s" % question_id)
+
+def results(request, question_id):
+    response = "you are looking at the result of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
